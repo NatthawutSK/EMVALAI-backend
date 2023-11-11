f191208e43a58a8289228da47a64179dd04bfb53
FixFire <89625074+NatthawutSK@users.noreply.github.com>
2023-11-11 15:02:26 +0700

    enable CORS in gateway

--- UserService/src/main/java/com/emvalai/userservice/UserServiceApplication.java
+++ UserService/src/main/java/com/emvalai/userservice/UserServiceApplication.java
@@ -3,11 +3,11 @@ package com.emvalai.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @EnableDiscoveryClient
 @SpringBootApplication
+
 public class UserServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);

c2884c197150908c4b01cd52d97d7a232e496721
Natayos <64070142@it.kmitl.ac.th>
2023-11-13 01:52:03 +0700

    Notification Service

---
+++ NotificationService/src/main/java/com/example/notificationservice/Services/NotificationService.java
@@ -0,0 +1,55 @@
+package com.example.notificationservice.Services;
+
+import com.example.notificationservice.Entity.NotificationEntity;
+import com.example.notificationservice.Repositories.NotificationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NotificationService {
+    @Autowired
+    private NotificationRepository notificationRepository;
+
+    public NotificationService(NotificationRepository notificationRepository){
+        this.notificationRepository = notificationRepository;
+    }
+
+    public List<NotificationEntity> getNotifications() {
+        return notificationRepository.findAll();
+    }
+
+    public NotificationEntity getNotificationByType(String type) {
+        return notificationRepository.findByType(type);
+    }
+
+    public NotificationEntity getNotificationByTitle(String title) {
+        return notificationRepository.findByTitle(title);
+    }
+
+    public NotificationEntity getNotificationById(String id) {
+        return notificationRepository.findBy_id(id);
+    }
+
+    public NotificationEntity createNotification(NotificationEntity notificationEntity) {
+        return notificationRepository.save(notificationEntity);
+    }
+
+//    public boolean deleteNotification(NotificationEntity notificationEntity) {
+//        try { notificationRepository.delete(notificationEntity); return true; }
+//        catch (Exception e){ return false;}
+//    }
+
+//    public NotificationEntity updateNotification(NotificationEntity notificationEntity) {
+//        NotificationEntity old_noti = notificationRepository.findByTitle(notificationEntity.getTitle());
+//        if (old_noti == null) {
+//            return null;
+//        }
+//        NotificationEntity updatedNoti = notificationRepository.save(notificationEntity);
+//        return updatedNoti;
+//    }
+    public NotificationEntity updateNotification(NotificationEntity notificationEntity) {
+            return notificationRepository.save(notificationEntity);
+        }
+}

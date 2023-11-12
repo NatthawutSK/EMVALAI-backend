757325f916549feb913778b560500b1bd8f2918c
FixFire <89625074+NatthawutSK@users.noreply.github.com>
2023-11-12 20:48:44 +0700

    add calendar service

---
+++ CalendarService/src/main/java/com/emvalai/calendarservice/query/CalendarEventHandler.java
@@ -0,0 +1,25 @@
+package com.emvalai.calendarservice.query;
+
+import com.emvalai.calendarservice.core.data.CalendarRepository;
+import com.emvalai.calendarservice.core.event.MeetingCreatedEvent;
+import org.axonframework.eventhandling.EventHandler;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CalendarEventHandler {
+    private final CalendarRepository calendarRepository;
+    public  CalendarEventHandler(CalendarRepository calendarRepository){
+        this.calendarRepository = calendarRepository;
+    }
+
+
+    @EventHandler
+    public void on(MeetingCreatedEvent meetingCreatedEvent){
+        System.out.println("in meeting event");
+//        System.out.println("in event created ");
+//        ProductEntity productEntity = new ProductEntity();
+//        BeanUtils.copyProperties(productCreatedEvent, productEntity);
+//        productRepository.save(productEntity);
+    }
+}
